5d86bb665817c9c00ff13d6e27fe9c54f641eb41
Estuardo <eramos.9588@gmail.com>
2020-12-19 16:40:10 -0600

    Adding comments on code and adding Adding comments on code and adding comparison between previous and new state on customhook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { useEffect, useState, useRef } from "react";
-import UseOnUpdate from "./useOnUpdate";
+/*importando el cusmtomHook*/
+import useOnUpdate from "./useOnUpdate";
 import "./styles.css";
 
 export default function App() {
+  /*dummyData funciona como valor de estado y setDummyData
+  functiona como su function propia para poderlo actualizar*/
+  /*en este caso se inicializa con un string que contiene la paralabra empty*/
   const [dummyData, setDummyData] = useState("empty");
+
+  /*useRef es un hook que nos permite persistir valores en todo el ciclo de vida
+ del componente*/
   let isMounted = useRef(false);
+
+  /*useEffect funciona tanto como componentDidMount
+  componentWillUmount y detecta los cambios en los estados de su 
+  lista de dependencia*/
+
+  /* La funcion dentro de useEffect se jecutara cuando
+  el comonente se monte, y la funcion en return cuando
+  este se vaya desmontar*/
+
+  /* el array [] indica las dependecias que necesita,
+  de estar vacio entonces indica que solo debe ejecutarse cuando
+  el componente sea montado y cuando se desmonte*/
+
+  /*si el array incluye variables, entonces tambien se ejecutara 
+  cuando un elemento del array de dependcias cambie*/
+
+  /*de no presentar el array, este siempre se ejecutara en cualquier
+  cambio del componente*/
 
   useEffect(() => {
     console.log("mounting lol");
@@ -13,22 +38,42 @@ export default function App() {
     };
   }, []);
 
+  /*en este punto useEffect recibe el estado de dummyData,
+  lo que le indica que ademas de actuar cuando el componente sea montado,
+  tambien actuara cuando dummyData Cambie*/
+
   useEffect(() => {
     console.log(`imrpimriemndo ${dummyData} desde useEffect`);
   }, [dummyData]);
 
+  /*El inconviente que presenta es que este siempre se ejecutara cuando el 
+  componente se monte lo cual puede causar problemas si se requiere llamar una
+  funcion que utiilice el estado y esta, debido a que esta recies definido no posea
+  los valores apropiados para ser usado*/
+
+  /*para eso se utiliza useRef lo cual permite persistir valores a lo largo de toda la vida
+  del componente, asi que se puede utilizar para identificar si el componente se esta montando
+  por primera vez y evitar llamar a la funcion o si este ya esta montado y asi solo se ejecutara
+  cuando sus dependecias cambien*/
+
   useEffect(() => {
-    if (isMounted.current) {
+    if (!isMounted.current) {
       isMounted.current = true;
     } else {
-      console.log(dummyData);
+      console.log(`imrpimriemndo ${dummyData} desde useEffect con useRef`);
     }
   }, [dummyData]);
 
-  UseOnUpdate(() => {
-    console.log(dummyData);
+  /*para evitar estar repitiendo la misma validacion en cada useEffect
+  podemos crear un customHook que se encargue de valdiar*/
+
+  /*al igual que useEffect este recibe una funcion y la dependencia*/
+  useOnUpdate(() => {
+    console.log(`imrpimriemndo ${dummyData} desde customHook useUpdate`);
   }, dummyData);
 
+  /*arrow function que se encarga cambiar el valor de dummyData cada vez
+  que se hace click al boton*/
   const addValue = () => setDummyData(Math.random());
 
   return (
--- src/useOnUpdate.js
+++ src/useOnUpdate.js
@@ -1,13 +1,41 @@
+/*CUSTOM HOOK DOCS: https://es.reactjs.org/docs/hooks-custom.html*/
+
+/*en resumen, un custom hook es un componente de react que utiliza
+useEffect para poder detectar efectos y poder definir acciones en base
+a validaciones que nosotros necesitemos*/
+
+/*las reglas principales de los CustomHooks son:
+1-siempre usar 'use' en el nombre de la funcion
+2-hace uso de useEffect
+3-siempre deben ser usadas en primer nivel
+*/
 import { useEffect, useRef } from "react";
 
-export default function UseOnUpdate(effect, dependency) {
+/*este custom hook recibe el valor que deseamos saber cuando
+se realice un cambio en el (dependency), y la function que deseamos llamar <effect>*/
+
+/*igualmetne usamos useRef para saber si el componente fue montado y llamar la funcion
+cuando la dependecia cambien*/
+
+/*adicionalmente usamos otro useRef guardar el valor previo de la dependencia y
+solo llamarla cuado su valor nuevo se diferente del previo*/
+
+export default function useOnUpdate(effect, dependency) {
   let isMounted = useRef(false);
+  let prevState = useRef(dependency);
 
   useEffect(() => {
-    if (isMounted.current) {
+    if (!isMounted.current) {
       isMounted.current = true;
     } else {
-      effect();
+      if (prevState !== dependency) {
+        effect();
+      }
     }
   }, [dependency]);
+  /*se puede utilizar un array con varias dependecias
+  pero provocara advertencia de eslint y no se cuales efectos podria causar*/
+
+  /*el warning que le hace falta effect en array de dependencias puede ser omitido,
+  ya que no es necesario para que el hook funcione*/
 }
